6c453d745e88c23d3fdec02eb803027d24a0f55d
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2018-07-17 23:41:22 +0600

    Add files via upload

---
+++ My template.cpp
@@ -0,0 +1,39 @@
+/**
+    Bismillahir Rahmanir Rahim
+    Md. Rakibul Islam
+    Dept. of CSE, CoU
+    **/
+#include<bits/stdc++.h>
+#define SQR(n) (n*n)
+#define CHR getchar()
+#define NL printf("\n")
+#define PI 2.0*acos(0.0)
+#define ll              long long int
+#define ull             unsigned long long int
+#define sc              scanf
+#define gcd(a,b)        __gcd(a,b)
+#define lcm(a,b)        ((a)*(b))/gcd(a,b)
+#define pf              printf
+#define MOD             1000000007
+#define mx              123456789
+#define Max(a,b) ((a>b)?a:b)
+#define Min(a,b) ((a<b)?a:b)
+#define ff first
+#define D double
+#define sz size()
+#define ss second
+#define PB push_back
+#define MP make_pair
+#define all(a) a.begin(),a.end()
+
+#define _Max(a,b,c) Max(a,Max(b,c))
+#define _Min(a,b,c) Min(a,Min(b,c))
+
+#define BitCnt(a) __builtin_popcountll(a)
+#define MEM(a,val) memset(a,val,sizeof(a))
+using namespace std;
+int main()
+{
+
+    return 0;
+}
