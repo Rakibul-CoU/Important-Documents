65fa94e1af8a8f9aed67c95d32664f569bd016fe
Md. Rakibul Islam <39064963+Rakibul-CoU@users.noreply.github.com>
2020-05-14 14:41:57 +0600

    Add files via upload

---
+++ Final_Template.cpp
@@ -0,0 +1,70 @@
+/*********************************
+****Bismillahir Rahmanir Raheem***
+*********************************/
+#include<bits/stdc++.h>
+
+/**LOOP**/
+#define test int t; cin>>t; while(t--)
+#define init(arr,val) memset(arr,val,sizeof(arr))
+#define loop(i,a,b) for(int i=a; i<b; i++)
+#define loopin(i,a,b) for(int i=a; i<=b; i++)
+#define loopdec(i,a,b) for(int i=a; i>=b; i--)
+#define loopsin(i,a,b,step) for(int i=a; i<b; i+=step)
+#define loopsdec(i,a,b,step) for(int i=a; i>=b; i-=step)
+
+#define pi 2.0*acos(0.0)
+#define ll long long int
+#define ull unsigned long long
+
+#define gcd(a,b)  __gcd(a,b)
+#define lcm(a,b)  ((a)*(b))/gcd(a,b)
+#define mod 1000000007
+#define mx 123456789
+
+/**STL**/
+#define pp pair
+#define ppi pair<int,int>
+#define ppl pair<long long int, long long int>
+#define ppu pair<unsigned long long int, unsigned long long int>
+#define lt list
+#define vec vector
+#define dq deque
+#define st set
+#define mst multiset
+#define mm map
+#define um unordered_map
+#define mp make_pair
+#define pb push_back
+#define pf push_front
+#define ff first
+#define ss second
+
+#define FAST ios_base::sync_with_stdio(false);cin.tie();cout.tie();
+#define file_read_in freopen("input.txt","r",stdin);
+#define file_read_out freopen("output.txt","w",stdout);
+
+/**SETPRECISION**/
+/*cout << fixed;
+  cout << setprecision();
+  cout<<ans<<endl*/
+using namespace std;
+
+
+/**Code goes here**/
+void solve()
+{
+
+}
+
+int main()
+{
+    FAST;
+    int t;
+    //cin>>t;
+    t=1;
+    while(t--)
+    {
+        solve();
+    }
+    return 0;
+}
